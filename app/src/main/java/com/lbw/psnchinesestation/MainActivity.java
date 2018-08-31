944faa34f4897f0619305599f58df19f94ae52db
Kobe Watermelon <704836663@qq.com>
2018-08-31 17:32:04 +0800

    添加drawable 和百度网页开启

--- app/src/main/java/com/lbw/psnchinesestation/MainActivity.java
+++ app/src/main/java/com/lbw/psnchinesestation/MainActivity.java
@@ -1,40 +1,67 @@
 package com.lbw.psnchinesestation;
 
+import android.media.PlaybackParams;
+import android.os.Build;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private WebView web;//网页控件
     private String URL = "http://psnine.com/";//网址
     private long exitTime = 0;//退出计时器
     private SwipeRefreshLayout swipe_refresh;//下拉刷新
+    private Button btn_bai_du;
+    private DrawerLayout dl;
+    private boolean flag=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        webSetting();
+
     }
 
+
+
     private void initView() {
+        String brand = android.os.Build.BRAND;
+       if (brand.equals("Xiaomi")){
+           flag=true;
+       }else {
+           flag=false;
+       }
+
         getSupportActionBar().hide();//去掉ActionBar
         web = findViewById(R.id.web_view);
+        dl=findViewById(R.id.drawer_layout);
+        btn_bai_du =findViewById(R.id.btn_baidu);
+        btn_bai_du.setOnClickListener(this);
+
         WebSettings webSettings = web.getSettings();
         webSettings.setJavaScriptEnabled(true);//支持JavaScript用于登录
         webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);//用于播放视频
         webSettings.setAppCacheEnabled(true);//开启缓存
-//        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//缓存策略
         web.requestFocus();//使页面获得焦点
         web.setWebChromeClient(new WebChromeClient());//使用webView打开网页
         web.setWebViewClient(new WebViewClient());    //使用webView打开网页
-        web.loadUrl(URL);//加载网页
+
         swipe_refresh = findViewById(R.id.swipe_refresh);
         swipe_refresh.setColorSchemeColors(getColor(R.color.statue_bar));
         //下拉刷新监听
@@ -46,6 +73,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void webSetting() {
+        web.loadUrl(URL);//加载网页
+    }
+    
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -53,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 web.goBack();
             } else {
                 if ((System.currentTimeMillis() - exitTime) > 2000) {
-                    Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(web,"再次点击退出!",Snackbar.LENGTH_SHORT).show();
                     exitTime = System.currentTimeMillis();
                 } else {
                     finish();
@@ -62,4 +93,24 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_baidu:
+               if (flag){
+                   URL="http://wap.luohua99.net/view/26218.html";
+               }else {
+                   URL="https://www.baidu.com";
+               }
+                webSetting();
+                dl.closeDrawer(GravityCompat.START);
+                break;
+
+
+        }
+    }
+
+
 }
