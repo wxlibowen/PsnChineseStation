233ebbdddea0e4cd9823c6e05ac922880a5ae7aa
LiBW <704836663@qq.com>
2018-10-13 14:50:26 +0800

    删除无用代码

--- app/src/main/java/com/lbw/psnchinesestation/MainActivity.java
+++ app/src/main/java/com/lbw/psnchinesestation/MainActivity.java
@@ -1,14 +1,11 @@
 package com.lbw.psnchinesestation;
 
-import android.media.PlaybackParams;
-import android.os.Build;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -17,7 +14,6 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
-import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private WebView web;//网页控件
